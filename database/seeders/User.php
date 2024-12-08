979f54d55d4bfecc65b58c19d78630d38884ac28
maulanaraka <maulanaraka611@gmail.com>
2024-12-08 22:07:26 +0700

    feat (database): update database and make user seeder

---
+++ database/seeders/User.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User as ModelsUser;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class User extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $userData = [
+            [
+                'name' => 'lord gopung',
+                'username' => 'admin',
+                'password' => bcrypt('admin'),
+                'role' => 'admin',
+            ],
+            [
+                'name' => 'man that operate',
+                'username' => 'operator',
+                'password' => bcrypt('operator'),
+                'role' => 'operator',
+            ],
+            [
+                'name' => 'mr smol',
+                'username' => 'staff',
+                'password' => bcrypt('staff'),
+                'role' => 'staff',
+            ]
+        ];
+
+        foreach($userData as $key => $val){
+            ModelsUser::create($val);
+        }
+    }
+}

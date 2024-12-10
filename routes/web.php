1b4fb3162ff25170c83e179070f9c998ceed17a3
maulanaraka <maulanaraka611@gmail.com>
2024-12-10 13:59:06 +0700

    feat(data-siswa page): make data siswa index for showing the table

--- routes/web.php
+++ routes/web.php
@@ -2,31 +2,37 @@
 
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\SessionController;
+use App\Http\Controllers\SiswaController;
 use App\Http\Middleware\UserAccess;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('landing');
 });
 
 // Authentication Middleware
-Route::middleware(['guest'])->group(function(){
-    Route::get('/login', [SessionController::class,'index'])->name('login');
-    Route::post('/login', [SessionController::class,'login']);
+Route::middleware(['guest'])->group(function () {
+    Route::get('/login', [SessionController::class, 'index'])->name('login');
+    Route::post('/login', [SessionController::class, 'login']);
 });
 
-// Route::get('/home',function(){
-//     return redirect('/operator');
-// });
-
-// Operator Routing
+// User Routing
 Route::middleware(['auth'])->group(function () {
-    Route::get('/admin', [UserController::class, 'admin'])->middleware('UserAccess:admin');
-    Route::get('/operator', [UserController::class, 'operator'])->middleware('UserAccess:operator');
-    Route::get('/staff', [UserController::class, 'staff'])->middleware('UserAccess:staff');
-    Route::get('/logout', [SessionController::class, 'logout']);
+    Route::get('/admin', [UserController::class, 'admin'])->middleware('UserAccess:admin')->name('admin');
+    Route::get('/operator', [UserController::class, 'operator'])->middleware('UserAccess:operator')->name('operator');
+    Route::get('/staff', [UserController::class, 'staff'])->middleware('UserAccess:staff')->name('staff');
+    Route::post('/logout', [SessionController::class, 'logout'])->name('logout');
 });
 
+// Staff - Siswa Routes
+Route::middleware(['auth', 'UserAccess:staff'])->group(function () {
+    Route::get('/siswa/{siswaID}/edit', [SiswaController::class, 'edit'])->name('siswa.edit');
+    Route::delete('/siswa/{siswaID}', [SiswaController::class, 'destroy'])->name('siswa.destroy');
+});
 
-
+// Data Siswa Page (Require Authentication)
+Route::middleware(['auth'])->group(function () {
+    Route::get('/data-siswa', [SiswaController::class, 'dataSiswa'])->name('data-siswa'); // Fixed path
+});
 
